fd0fd0bd8d5da4a904f4385d25230814c42e39be
ritanrmailn <95425842+ritanrmailn@users.noreply.github.com>
2021-12-04 15:15:18 +0700

    update UI login form

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+/* This will give an error. Note the output
+ * above, which is before the header() call */
+header('Location: login.php');
+exit;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
